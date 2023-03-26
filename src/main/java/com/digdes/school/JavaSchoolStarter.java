5c03175bbda5e4660d9c058015a8f742fe8a6402
Nikita Markovskiy <markovskinikita@gmail.com>
2023-03-26 18:39:30 +0300

    +JavaSchoolStarter

---
+++ src/main/java/com/digdes/school/JavaSchoolStarter.java
@@ -0,0 +1,17 @@
+package com.digdes.school;
+
+import java.util.List;
+import java.util.Map;
+
+public class JavaSchoolStarter {
+    public JavaSchoolStarter() {
+
+    }
+    public List<Map<String,Object>> execute(String request) throws Exception {
+        //Здесь начало исполнения вашего кода
+
+        //  return new ArrayList<>();
+        return null;
+    }
+
+}
